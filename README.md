9d5ed6084ffc755bd6a3eb5724050a008ebf8722
spleennooname <spleen666@gmail.com>
2019-10-27 15:52:23 +0100

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-"# sparkar-fractura" 
+![](https://img.shields.io/badge/sdk-v73-green) [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
+
+# spark-safety-glass
+> script version of [Josh Beckwith](https://github.com/positlabs) [sparkar-safety-glass](https://github.com/positlabs/spark-safety-glass)
+
+## setup
+
+* install deps `npm i`
+* webpack watching `npm run dev`
+* webpack build `npm run build`
+
+source: [script](./src/index.js)
---
+++ src/index.js
@@ -0,0 +1,39 @@
+
+/**
+ *
+ *
+ */
+
+const Materials = require('Materials')
+const Textures = require('Textures')
+const Patches = require('Patches')
+const Shaders = require('Shaders')
+const CameraInfo = require('CameraInfo')
+const R = require('Reactive')
+const console = require('Diagnostics')
+//
+const faceCameraMaterial = Materials.get('face0')
+// get resolution
+const res = R.pack2(CameraInfo.previewSize.width, CameraInfo.previewSize.height)
+// get camera shader signal
+const cameraColor = Textures.get('cameraTexture0').signal
+const texc = Shaders.vertexAttribute({ variableName: Shaders.VertexAttribute.TEX_COORDS })
+// get per-fragment uv
+const uv = Shaders.fragmentStage(texc)
+
+const cell = 0.05
+const strength = 2
+
+const newUV = uv
+  .mod(cell)
+  .mul(strength)
+  .add(
+    R.val(cell)
+      .mul(
+        R.sign(strength).mul(R.val(-1))
+      )
+  )
+// sampling texture color at uv coords
+const finalColor = Shaders.textureSampler(cameraColor, texc.add(newUV))
+// Assign the shader signal to the texture slot
+faceCameraMaterial.setTexture(finalColor, { textureSlotName: Shaders.DefaultMaterialTextures.DIFFUSE })
